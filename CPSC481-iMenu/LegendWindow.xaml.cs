7e37edec88fafc0d0eaf9aca6b346fbe22ffc996
simratbenipal <117254852+simratbenipal@users.noreply.github.com>
2023-03-09 11:26:05 -0700

    legand window created

---
+++ CPSC481-iMenu/LegendWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CPSC481_iMenu
+{
+    /// <summary>
+    /// Interaction logic for LegendWindow.xaml
+    /// </summary>
+    public partial class LegendWindow : Window
+    {
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
